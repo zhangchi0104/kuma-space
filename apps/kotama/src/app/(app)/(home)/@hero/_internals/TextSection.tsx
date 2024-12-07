eecb6af9792526013e53f3b488aec1a86b7dc9b8
Alex Zhang <zhangchi0104@live.com>
2024-12-07 20:10:07 +1100

    fix(kotama:home): added i18n for greetings

--- apps/kotama/src/app/(app)/(home)/@hero/_internals/TextSection.tsx
+++ apps/kotama/src/app/(app)/(home)/@hero/_internals/TextSection.tsx
@@ -1,28 +1,30 @@
 /** @format */
 
-import AnimatedText from "@/src/components/AnimatedText";
-import { BaseStyleProps } from "@/src/utils/typings";
-import clsx from "clsx";
+import AnimatedText from '@/src/components/AnimatedText';
+import { BaseStyleProps } from '@/src/utils/typings';
+import clsx from 'clsx';
+import { useTranslations } from 'next-intl';
 // import AnimatedText from '~/components/AnimatedText';
 type TextSectionProps = BaseStyleProps & {
   duration?: number;
 };
 const TextSection: React.FC<TextSectionProps> = ({ className, duration }) => {
+  const t = useTranslations('Home.Hero');
   const greetingStyle = clsx(
-    "text-2xl",
-    "md:text-4xl",
-    "sm:text-3xl",
-    "whitespace-pre",
-    "leading-[2rem]",
-    "md:leading-[3.5rem]",
-    "pb-4",
-    className,
+    'text-2xl',
+    'md:text-4xl',
+    'sm:text-3xl',
+    'whitespace-pre',
+    'leading-[2rem]',
+    'md:leading-[3.5rem]',
+    'pb-4',
+    className
   );
   return (
-    <div className="text-center sm:text-left">
+    <div className='text-center sm:text-left'>
       <AnimatedText
         className={greetingStyle}
-        text={"G'day I'm Alex.\nA fullstack developer"}
+        text={t('greeting')}
         duration={duration}
         hideCursorAfterAnimation
       />
