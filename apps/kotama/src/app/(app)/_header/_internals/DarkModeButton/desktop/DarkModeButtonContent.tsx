aeec45eff5094e8fd831481699e138d917b5d668
Alex Zhang <zhangchi0104@live.com>
2024-12-22 16:11:01 +1100

    feat: disable text animation after played

--- apps/kotama/src/app/(app)/_header/_internals/DarkModeButton/desktop/DarkModeButtonContent.tsx
+++ apps/kotama/src/app/(app)/_header/_internals/DarkModeButton/desktop/DarkModeButtonContent.tsx
@@ -3,24 +3,35 @@
 'use client';
 import { DropdownMenuItem } from '@/src/components/ui/dropdown-menu';
 import { DropdownMenuContent } from '@/src/components/ui/dropdown-menu';
+import { cn } from '@/src/utils/shadcn';
 import { DesktopIcon, MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { useTranslations } from 'next-intl';
 import { useTheme } from 'next-themes';
 
 const DarkModeButtonContent = () => {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
   const t = useTranslations('Header.DarkModeToggle');
+
   return (
     <DropdownMenuContent align='end'>
-      <DropdownMenuItem onClick={() => setTheme('light')}>
+      <DropdownMenuItem
+        className={cn(theme === 'light' && 'font-semibold')}
+        onClick={() => setTheme('light')}
+      >
         <SunIcon className='w-4 h-4 mr-2' />
         <p>{t('light')}</p>
       </DropdownMenuItem>
-      <DropdownMenuItem onClick={() => setTheme('dark')}>
+      <DropdownMenuItem
+        className={cn(theme === 'dark' && 'font-semibold')}
+        onClick={() => setTheme('dark')}
+      >
         <MoonIcon className='w-4 h-4 mr-2' />
         <p>{t('dark')}</p>
       </DropdownMenuItem>
-      <DropdownMenuItem onClick={() => setTheme('system')}>
+      <DropdownMenuItem
+        className={cn(theme === 'system' && 'font-semibold')}
+        onClick={() => setTheme('system')}
+      >
         <DesktopIcon className='w-4 h-4 mr-2' />
         <p>{t('system')}</p>
       </DropdownMenuItem>
