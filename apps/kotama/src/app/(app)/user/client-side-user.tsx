80b98bfef018ab8dda1ad7ec93b1ed6278afe2c1
Alex Zhang <39239965+zhangchi0104@users.noreply.github.com>
2025-06-08 10:40:54 +1000

    fix: lint errors

--- apps/kotama/src/app/(app)/user/client-side-user.tsx
+++ apps/kotama/src/app/(app)/user/client-side-user.tsx
@@ -2,14 +2,14 @@
 
 import { createClientSideSupabaseClient } from "@/src/lib/supabase/client";
 import { UserResponse } from "@supabase/supabase-js";
-import { use, useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 const ClientSideUser = () => {
   const supabaseClient = useMemo(() => createClientSideSupabaseClient(), []);
   const [user, setUser] = useState<UserResponse | null>(null);
   useEffect(() => {
     supabaseClient.auth.getUser().then((userSession) => setUser(userSession));
-  }, []);
+  }, [supabaseClient.auth]);
   return (
     <div>
       <p>{JSON.stringify(user, null, 2)}</p>
